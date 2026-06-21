16378eb2fc71e7a363c9e3b5af66bd7aa3a9ad6b
Abdalla420 <mamanbusiness420@gmail.com>
2026-06-21 07:47:05 -0400

    Add get and put to title.js

---
+++ server/routes/title.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const Title = require("../models/Title.js");
+
+router.get("/", (req, res) => {
+    try {
+        const title = await Title.findOne();
+        if(!title){
+            res.status(404).json({message: "Title not found"});
+        }
+        res.status(200).json(title);
+    }catch (error) {
+        res.status(500).json({message: "Server error"});
+    };
+});
+
+router.put("/", async (req, res) => {
+    try {
+        const { content } = req.body;
+        // note to my self, {} since there is only one title, { new: true } tells mongoose to return update document
+        const title = await Title.findOneAndUpdate({}, { content }, { new: true });
+        res.status(200).json(title);
+    } catch(error) {
+        res.status(500).json({message: "Server error"});
+    };
+});
+
+module.exports = router;
