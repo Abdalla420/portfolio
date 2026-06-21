14a8debd2f9d97cffb71b4968dc627f4f56741f0
Abdalla420 <mamanbusiness420@gmail.com>
2026-06-21 13:39:14 -0400

    Add space because of ocd

--- server/models/Title.js
+++ server/models/Title.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const titleSchema = new mongoose.Schema({
     content: {type: String, maxlength: 50}
-}, {timestamps:true});
+}, {timestamps: true});
 
 
 module.exports = mongoose.model("Title", titleSchema);
