81282e38d49053a6c76b62b4abcee70dd077acde
Abdalla420 <mamanbusiness420@gmail.com>
2026-06-22 12:33:57 -0400

    Finish project.js routes

---
+++ server/routes/project.js
@@ -0,0 +1,55 @@
+const express = require("express");
+
+const Project = require("../models/Project");
+
+const router = express.Router();
+
+
+router.post("/", async (req, res) => {
+    try{
+        const {text, image, link} = req.body;
+        const project = await Project.create({text, image, link});
+        res.status(201).json(project);
+    }catch(error){
+        res.status(500).json({message: "Server error"});
+    }
+})
+router.get("/", async (req, res) => {
+    try{
+        const project = await Project.find();
+        if(!project || project.length === 0){
+            return res.status(404).json({message: "Project not found"});
+        }
+        res.status(200).json(project);
+    }catch(error){
+        res.status(500).json({message: "Server error"});
+    }
+});
+router.put("/:id", async (req, res) => {
+    try{
+        const {text, image, link} = req.body;
+        const project = await Project.findByIdAndUpdate(req.params.id, {text, image, link}, {new: true});
+        if(!project){
+            res.status(404).json({message: "Project not found"});
+        };
+        res.status(200).json(project);
+    }catch(error){
+        res.status(500).json({message: "Server error"});
+    }
+})
+router.delete("/:id", async (req, res) => {
+    try{
+        const project = await Project.findByIdAndDelete(req.params.id);
+        if(!project){
+            return res.status(404).json({message: "Project not found"});
+        };
+        res.status(200).json(project);
+    }catch(error){
+        res.status(500).json({message: "Server error"})
+    }
+});
+
+
+
+
+module.exports = router;
