d9123116c87f6db583ec33c73222bd416bb555d8
Abdalla420 <mamanbusiness420@gmail.com>
2026-06-22 12:34:38 -0400

    Add project routes

--- server/server.js
+++ server/server.js
@@ -18,11 +18,14 @@ const greetingRouter = require("./routes/greeting");
 const aboutRouter = require("./routes/about");
 const locationRouter = require("./routes/location");
 const skillRouter = require("./routes/skill");
+const projectRouter = require("./routes/project");
 
 app.use("/api/title", titleRouter);
 app.use("/api/greeting", greetingRouter);
 app.use("/api/about", aboutRouter);
 app.use("/api/location", locationRouter);
 app.use("/api/skill", skillRouter);
+app.use("/api/project", projectRouter);
+
 
 app.listen(port, _ => console.log(`Server started at ${port}`));
