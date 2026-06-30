09116181c859cbd43f329db9a782e2cee3ac11a5
Abdalla420 <mamanbusiness420@gmail.com>
2026-06-30 06:37:35 -0400

    Add header to admin.html

--- portfolio/src/app/admin/admin.ts
+++ portfolio/src/app/admin/admin.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
-
+import { Shared } from './shared/shared';
 
 @Component({
   selector: 'app-admin',
-  imports: [RouterOutlet, RouterLinkWithHref],
+  imports: [RouterOutlet, Shared],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
